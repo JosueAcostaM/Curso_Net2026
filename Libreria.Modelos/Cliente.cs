8db6f6c27ba04c1d01bd8aeb1fc792c17819840a
Josue <josuem3693@gmail.com>
2026-03-17 09:08:13 -0500

    Agregar Nuevos Modelos Cliente y Pais

---
+++ Libreria.Modelos/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Modelos
+{
+    public class Cliente
+    {
+        [Key] public int Id { get; set; }
+
+        public string Nombre_Cliente { get; set; }
+        public string Apellido_Cliente { get; set; }
+        public string Correo_Cliente { get; set; }
+
+        public string Nombre_Usuario { get; set; }
+        public string Contrasena_Cliente { get; set; }
+    }
+}
---
+++ Libreria.Modelos/Pais.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Modelos
+{
+    public class Pais
+    {
+        [Key] public int Id { get; set; }
+
+        public string Nombre_Pais { get; set; }
+
+    }
+}
