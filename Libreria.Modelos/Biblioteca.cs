094fe224aedf4b78837e2e092617f7470451b3ae
Josue <josuem3693@gmail.com>
2026-03-17 08:57:17 -0500

    Agregar archivos de proyecto.

---
+++ Libreria.Modelos/Biblioteca.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Modelos
+{
+    public class Biblioteca
+    {
+        //Llave primaria
+        [Key] public int Id { get; set; }
+
+        public string Nombre_Biblioteca { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
---
+++ Libreria.Modelos/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Libreria.Modelos
+{
+    public class Class1
+    {
+
+    }
+}
